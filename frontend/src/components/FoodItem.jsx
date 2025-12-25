d94e0ab2330bed7a143f501612cb9ed2c7c9f750
samir-27 <sumrasamir27@gmail.com>
2025-12-25 12:44:23 +0530

    cart functionality added

--- frontend/src/components/FoodItem.jsx
+++ frontend/src/components/FoodItem.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaStar } from "react-icons/fa";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { StoreContext } from "../context/StoreContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
+    const {cartItems,addToCart,removeFromCart} = useContext(StoreContext)
     return (
         <div className="bg-white rounded-2xl shadow-md hover:shadow-xl transition-shadow duration-300 overflow-hidden cursor-pointer s:w-full w-60">
 
@@ -11,6 +14,7 @@ const FoodItem = ({ id, name, price, description, image }) => {
                     alt={name}
                     className="h-full w-full object-cover hover:scale-110 transition-transform duration-500"
                 />
+
             </div>
             <div className="p-4 space-y-2">
                 <div className="flex items-center justify-between">
@@ -28,13 +32,30 @@ const FoodItem = ({ id, name, price, description, image }) => {
                 <p className="text-sm text-gray-500 line-clamp-2">
                     {description}
                 </p>
-                <div className="flex items-center justify-between pt-2">
+                <div className="flex items-center justify-between pt-2 ">
                     <p className="text-lg font-bold text-orange-600">
                         ₹{price}
                     </p>
-                    <button className="bg-orange-600 text-white text-sm px-3 py-1 rounded-full hover:bg-orange-700 transition">
+                    {!cartItems[id]
+                    ?
+                    <div className="bg-gray-100 p-1 rounded-full">
+                    <button onClick={()=>addToCart(id)} className="bg-green-600 text-white text-sm px-3 py-1 rounded-full hover:bg-green-700 transition ">
                         Add
                     </button>
+                    </div>
+                    :
+                    <div className="flex gap-5 bg-gray-100 p-1 rounded-full">
+
+                    <button onClick={()=>removeFromCart(id)} className="bg-orange-600 text-white text-sm px-3 py-1 rounded-full hover:bg-orange-700 transition">
+                        Remove
+                    </button>
+                    <p>{cartItems[id]}</p>
+                     <button onClick={()=>addToCart(id)} className="bg-green-600 text-white text-sm px-3 py-1 rounded-full hover:bg-green-700 transition">
+                        Add
+                    </button>
+                    </div>
+                    }
+
                 </div>
             </div>
         </div>
